897ca33a6bebcccf413b01247f547eaa87f4406d
Cezary Petryka <cezaryp10@gmail.com>
2024-02-17 14:25:51 +0100

    Add basic Spring configuration

---
+++ src/main/java/com/app/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.app.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.app")
+public class AppConfig {
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+    }
+}
