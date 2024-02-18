05d027a528053488ff5ed1c4f337a6da1cbacfb6
Cezary Petryka <cezaryp10@gmail.com>
2024-02-18 09:55:10 +0100

    Add an implementation of a validation layer

--- src/main/java/com/app/config/AppConfig.java
+++ src/main/java/com/app/config/AppConfig.java
@@ -6,11 +6,13 @@ import com.google.gson.GsonBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.time.LocalDate;
 
 @Configuration
 @ComponentScan("com.app")
+@PropertySource("classpath:application.properties")
 public class AppConfig {
     @Bean
     public Gson gson() {
