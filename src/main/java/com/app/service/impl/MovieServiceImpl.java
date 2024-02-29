6f818409da52b0ca1b01a4047a95df1650073564
Cezary Petryka <cezaryp10@gmail.com>
2024-02-29 18:07:54 +0100

    Add documentation of the created methods

--- src/main/java/com/app/service/impl/MovieServiceImpl.java
+++ src/main/java/com/app/service/impl/MovieServiceImpl.java
@@ -17,6 +17,15 @@ import java.util.stream.Collectors;
 public class MovieServiceImpl implements MovieService {
     private final MovieRepository movieRepository;
 
+    /**
+     * Sorts a list of {@code Movie} objects based on the provided criterion.
+     *
+     * @param movieComparator The criterion for sorting {@code Movie} objects. It's an object implementing the
+     *                        {@link Comparator<Movie>} interface, which defines how two {@code Movie} objects
+     *                        are compared to each other.
+     * @return A list of {@code Movie} objects sorted based on the given criterion.
+     * @throws IllegalArgumentException if the provided movie comparator is null.
+     */
     @Override
     public List<Movie> sortBy(Comparator<Movie> movieComparator) {
         if(movieComparator == null) {
@@ -30,6 +39,16 @@ public class MovieServiceImpl implements MovieService {
                 .toList();
     }
 
+    /**
+     * Counts the number of {@code Movie} objects based on the provided classifier.
+     *
+     * @param <T>        the type of the key in the resulting map, determined by the classifier's output.
+     * @param classifier a {@link Function} interface that takes a {@code Movie} object and returns
+     *                   a value of type {@code T} that will be used as a key in the resulting map.
+     * @return A map with keys of type {@code T} and values representing the number of {@code Movie}
+     *         objects that match each key.
+     * @throws IllegalArgumentException if the provided classifier is null.
+     */
     @Override
     public <T> Map<T, Long> countBy(Function<Movie, T> classifier) {
         if(classifier == null) {
