a5a72be084788c82f20834a7df958510a7fef832
Cezary Petryka <cezaryp10@gmail.com>
2024-03-09 14:39:57 +0100

    Add a bean to send emails

--- src/main/java/com/app/config/AppConfig.java
+++ src/main/java/com/app/config/AppConfig.java
@@ -3,22 +3,43 @@ package com.app.config;
 import com.app.json.deserializer.custom.LocalDateDeserializer;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import lombok.RequiredArgsConstructor;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+import org.simplejavamail.mailer.MailerBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import java.time.LocalDate;
 
 @Configuration
 @ComponentScan("com.app")
 @PropertySource("classpath:application.properties")
+@RequiredArgsConstructor
 public class AppConfig {
+    private Environment environment;
+
     @Bean
     public Gson gson() {
         return new GsonBuilder()
                 .setPrettyPrinting()
                 .registerTypeAdapter(LocalDate.class, new LocalDateDeserializer())
                 .create();
+    }
+
+    @Bean
+    public Mailer mailer() {
+        return MailerBuilder
+                .withSMTPServer(
+                        environment.getRequiredProperty("email.host"),
+                        environment.getRequiredProperty("email.port", Integer.class),
+                        environment.getRequiredProperty("email.username"),
+                        environment.getRequiredProperty("email.password"))
+                .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                .async()
+                .buildMailer();
     }
 }
