0444bec8cccf7c4eed71d81e59ebc0365504ca39
Cezary Petryka <cezaryp10@gmail.com>
2024-03-18 15:20:44 +0100

    Prepare a final version of the main method, including example calls of all implemented methods

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -1,15 +1,19 @@
 package com.app;
 
 import com.app.config.AppConfig;
-import com.app.json.deserializer.impl.MoviesDataJsonDeserializer;
-import com.app.repository.impl.MovieRepositoryImpl;
+import com.app.service.impl.MovieServiceImpl;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
+        // Create a new context that will be used to get beans from the container
         var context = new AnnotationConfigApplicationContext(AppConfig.class);
-        var movieRepositoryImpl
-                = context.getBean("movieRepositoryImpl", MovieRepositoryImpl.class);
-        System.out.println(movieRepositoryImpl.getMovies());
+
+        // Get the MovieRepositoryImpl bean from the container
+        var movieService = context.getBean("movieServiceImpl", MovieServiceImpl.class);
+
+        // Invoke methods generating a report about movies and then send it by email and save as a PDF file
+        movieService.sendReportByEmail("cezaryp10@gmail.com", "Complete movies report");
+        movieService.saveReportAsPdf("movies-report.pdf");
     }
 }
