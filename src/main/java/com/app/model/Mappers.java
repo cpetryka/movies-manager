5ef75ad2df074af3fd8a63f47255de856149ed66
Cezary Petryka <cezaryp10@gmail.com>
2024-03-24 22:05:50 +0100

    Add mappers for all remaining fields

--- src/main/java/com/app/model/Mappers.java
+++ src/main/java/com/app/model/Mappers.java
@@ -5,10 +5,12 @@ import java.util.List;
 import java.util.function.Function;
 
 public interface Mappers {
+    Function<Movie, String> toTitleMapper = movie -> movie.title;
     Function<Movie, Genre> toGenreMapper = movie -> movie.genre;
     Function<Movie, String> toDirectorMapper = movie -> movie.director;
     Function<Movie, LocalDate> toReleaseDataMapper = movie -> movie.releaseDate;
     Function<Movie, List<String>> toCastMapper = movie -> movie.cast;
     Function<Movie, Integer> toDurationMapper = movie -> movie.duration;
     Function<Movie, Double> toRatingMapper = movie -> movie.rating;
+    Function<Movie, String> toImdbIdMapper = movie -> movie.imdbId;
 }
