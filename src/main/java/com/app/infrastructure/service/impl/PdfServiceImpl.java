96765a8fff32fc90eb056baf8b0d30eba3ce75e9
Cezary Petryka <cezaryp10@gmail.com>
2024-11-21 18:44:33 +0100

    Add the generation of relevant logs after the creation of a PDF file

--- src/main/java/com/app/infrastructure/service/impl/PdfServiceImpl.java
+++ src/main/java/com/app/infrastructure/service/impl/PdfServiceImpl.java
@@ -3,11 +3,13 @@ package com.app.infrastructure.service.impl;
 import com.app.application.service.PdfService;
 import com.itextpdf.html2pdf.HtmlConverter;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.FileOutputStream;
 
 @Service
+@Slf4j(topic = "MainAsyncLogger")
 public class PdfServiceImpl implements PdfService {
     @SneakyThrows
     @Override
@@ -24,5 +26,7 @@ public class PdfServiceImpl implements PdfService {
                 htmlContent,
                 new FileOutputStream(pdfFilePath)
         );
+
+        log.info("A PDF file has been created");
     }
 }
